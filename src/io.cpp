5fa49728fa1097fa26f480b90f43abe1f884410b
triqadafi <mail@triqadafi.com>
2018-01-19 16:05:40 +0700

    move adc

--- src/io.cpp
+++ src/io.cpp
@@ -5,9 +5,6 @@ void fi_IOClass::init(){
   pinMode(PIN_BIN_1, OUTPUT); digitalWrite(PIN_BIN_1, LOW);
   pinMode(PIN_NAV_R, INPUT_PULLUP);
   pinMode(PIN_NAV_L, INPUT_PULLUP);
-
-  // Disable what we don't need to save power
-  ADCSRA &= ~(1<<ADEN); // Disable ADC
 }
 void fi_IOClass::output(){
   is_input = false;
