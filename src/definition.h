867ea34eee5d975c03b8402a264ac60caf71ed4f
triqadafi <mail@triqadafi.com>
2018-01-19 16:05:54 +0700

    add new function

--- src/definition.h
+++ src/definition.h
@@ -6,6 +6,8 @@
 #define FI_D_MENU_MINUTE 2
 #define FI_D_MENU_MENU 3
 #define FI_D_MENU_SETTING 4
+#define FI_D_MENU_PROGRAM 98
+#define FI_D_MENU_SLEEP 99
 
 #define FI_D_RIGHT 1
 #define FI_D_LEFT 2
