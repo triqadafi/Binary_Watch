48c477ed64b2dc7a58b4a85e6cd0737596fdf0a5
triqadafi <mail@triqadafi.com>
2018-01-19 16:05:25 +0700

    last working

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 #include <TinyWireM.h>                  // I2C Master lib for ATTinys which use USI
 #include <TinyRTClib.h>
 
@@ -11,67 +13,78 @@
 RTC_DS1307 rtc;                      // Set up real time clock
 
 unsigned long FI_G_MILLIS = 0;
-uint8_t FI_G_MENU = 0;
-uint8_t FI_G_MENU_STATE = 0;
+volatile uint8_t FI_G_MENU = 0;
+volatile uint8_t FI_G_MENU_STATE = 0;
+volatile uint8_t FI_G_MENU_SLEEP_CALLBACK = 0;
 uint8_t FI_G_MENU_CHILD = 0;
 
 uint8_t FI_G_SETTING_HOUR = 0;
 uint8_t FI_G_SETTING_MINUTE = 0;
 
-void setup() {
+void fi_sleep_init(){
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+}
+void enter_sleep(){
+  sleep_enable();
+  sleep_cpu();
+}
 
-  TinyWireM.begin();            // Begin I2C
-  rtc.begin();                  // Begin DS1307 real time clock
+void fi_adc_disable(){
+  ADCSRA &= ~(1<<ADEN);                                                         // Disable ADC
+}
 
-  if (! rtc.isrunning()) {      // Uncomment lines below first use of clock to set time
-    //Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(__DATE__, __TIME__));
-    //rtc.adjust(DateTime(2014, 1, 21, 7, 0, 0));
+void fi_interrupt_enable(){
+  GIMSK = 0b00100000;
+  PCMSK = 0b00000101; // pb 0 1 4
+  sei();
+}
 
+void fi_interrupt_disable(){
+  // cli();
+  GIMSK = 0b00000000;
+  PCMSK = 0b00000000; // pb 0 1 4
+}
+
+ISR(PCINT0_vect){
+  if(FI_NAV_R){
+    FI_G_MENU = FI_D_MENU_HOME;
+    FI_G_MENU_STATE = 0;
+    FI_G_MENU_SLEEP_CALLBACK = 1;
   }
-    // put your setup code here, to run once:
+  if(FI_NAV_L){
+    FI_G_MENU = FI_D_MENU_HOME;
+    FI_G_MENU_STATE = 0;
+    FI_G_MENU_SLEEP_CALLBACK = 2;
+  }
+}
+void setup() {
+  TinyWireM.begin();                                                            // Begin I2C
+  rtc.begin();                                                                  // Begin DS1307 real time clock
+
+  if(!rtc.isrunning()){
+    rtc.adjust(DateTime(__DATE__, __TIME__));                                   // following line sets the RTC to the date & time this sketch was compiled
+    // rtc.adjust(DateTime(2017, 1, 21, 7, 0, 0));
+  }
+
+  fi_sleep_init();
   fi_IO.init();
   fi_IO.input();
 
-  // FI_G_MILLIS = millis();
-  // while (1) {
-  //   if(millis() - FI_G_MILLIS < 100){
-  //     fi_Interface.set(1, 1);
-  //   }else if(millis() - FI_G_MILLIS < 100*2){
-  //     fi_Interface.set(0, 0);
-  //   }else if(millis() - FI_G_MILLIS < 100*3){
-  //     fi_Interface.set(1, 1);
-  //   }else if(millis() - FI_G_MILLIS < 100*13){
-  //     fi_Interface.set(0, 0);
-  //   }else if(millis() - FI_G_MILLIS < 100*14){
-  //     fi_Interface.set(1, 1);
-  //     FI_G_MILLIS = millis();
-  //   }
-  //
-  //   if(FI_NAV_R){
-  //     fi_Interface.set(1, 1);
-  //     fi_IO.safe(FI_D_RIGHT);
-  //     fi_Interface.set(0, 0);
-  //
-  //     break;
-  //   }
-  // }
+  fi_adc_disable();
 
-  fi_IO.safe(FI_D_RIGHT);
-  fi_IO.output();
+  FI_G_MENU = FI_D_MENU_SLEEP;
+  FI_G_MENU_STATE = 0;
 }
 
 uint8_t hourvalue, minutevalue, precisionvalue;
 void loop() {
   if(FI_G_MENU == FI_D_MENU_HOME){
     if(FI_G_MENU_STATE == 0){
-      fi_IO.input();
-
+      fi_interrupt_disable();
       FI_G_MENU_STATE++;
     }
     if(FI_G_MENU_STATE == 1){
-        if(FI_NAV_R){
+        if(FI_G_MENU_SLEEP_CALLBACK == 1){
           fi_Interface.set(0, 1);
           fi_IO.safe(FI_D_RIGHT);
           fi_Interface.set(0, 0);
@@ -80,7 +93,7 @@ void loop() {
           FI_G_MENU_STATE = 0;
           FI_G_MENU = FI_D_MENU_HOUR;
         }
-        if(FI_NAV_L){
+        if(FI_G_MENU_SLEEP_CALLBACK == 2){
           fi_Interface.set(1, 0);
           fi_IO.safe(FI_D_LEFT);
           fi_Interface.set(0, 0);
@@ -103,6 +116,7 @@ void loop() {
     if(FI_G_MENU_STATE == 0){
       fi_IO.output();
       DateTime now = rtc.now();           // Get the RTC info
+      fi_IO.input();
 
       hourvalue = now.hour();             // Get the hour
       if(hourvalue > 12) hourvalue -= 12; // This clock is 12 hour, is 13-24, convert to 1-12
@@ -117,9 +131,43 @@ void loop() {
       fi_Binary.precision(precisionvalue);
 
       FI_G_MENU_STATE = 0;
-      FI_G_MENU = FI_D_MENU_HOME;
+      if(FI_NAV_R){
+        fi_Interface.set(1, 1);
+        fi_IO.safe(FI_D_RIGHT);
+        delay(1000);
+        fi_Interface.set(0, 0);
+        FI_G_MENU = FI_D_MENU_PROGRAM;
+      }else{
+        FI_G_MENU = FI_D_MENU_SLEEP;
+      }
     }
-  }else if(FI_G_MENU == FI_D_MENU_MINUTE){
+  }
+  else if(FI_G_MENU == FI_D_MENU_PROGRAM){
+    if(FI_G_MENU_STATE == 0){
+      for (uint8_t i = 0; i < 5; i++) {
+        fi_Interface.set(1, 1);
+        delay(100);
+        fi_Interface.set(0, 0);
+      }
+      FI_G_MENU_STATE++;
+    }
+    if(FI_G_MENU_STATE == 1){
+      fi_interrupt_disable();
+      fi_IO.output();
+    }
+  }
+  else if(FI_G_MENU == FI_D_MENU_SLEEP){
+    if(FI_G_MENU_STATE == 0){
+      delay(800);
+      fi_interrupt_enable();
+      delay(200);
+      FI_G_MENU_STATE++;
+    }
+    if(FI_G_MENU_STATE == 1){
+      enter_sleep(); // actually this has an internal loop
+    }
+  }
+  else if(FI_G_MENU == FI_D_MENU_MINUTE){
     if(FI_G_MENU_STATE == 0){
       fi_IO.output();
       DateTime now = rtc.now();           // Get the RTC info
@@ -142,7 +190,7 @@ void loop() {
         fi_Interface.set(0, 0);
         FI_G_MENU = FI_D_MENU_MENU;
       }else{
-        FI_G_MENU = FI_D_MENU_HOME;
+        FI_G_MENU = FI_D_MENU_SLEEP;
       }
     }
   }else if(FI_G_MENU == FI_D_MENU_MENU){
@@ -276,7 +324,7 @@ void loop() {
         if(FI_G_MENU_CHILD == 5){
           FI_G_MENU = FI_D_MENU_SETTING;
         }else{
-          FI_G_MENU = FI_D_MENU_HOME;
+          FI_G_MENU = FI_D_MENU_SLEEP;
         }
         delay(1000);
       }
@@ -324,6 +372,15 @@ void loop() {
         fi_Interface.set(0, 0);
         delay(1000);
 
+        fi_IO.output();
+        DateTime now = rtc.now();           // Get the RTC info
+        fi_IO.input();
+
+        FI_G_SETTING_MINUTE = now.minute() / 5;             // Get the hour
+        if(FI_G_SETTING_MINUTE > 12) FI_G_SETTING_MINUTE -= 12; // This clock is 12 hour, is 13-24, convert to 1-12
+        fi_Binary.set(FI_G_SETTING_MINUTE);
+        fi_Interface.set(0, 1);
+
         FI_G_MENU_STATE = 3;
       }
     }
@@ -341,8 +398,12 @@ void loop() {
       }
       if(FI_NAV_L){
         fi_Interface.set(0, 0);
-        fi_IO.safe(FI_D_LEFT);
         delay(1000);
+        fi_Interface.set(1, 1);
+        delay(200);
+        fi_Interface.set(0, 0);
+        delay(500);
+        fi_IO.safe(FI_D_LEFT);
         fi_Binary.set(FI_G_SETTING_HOUR);
         delay(1000);
         FI_G_MILLIS = millis();
@@ -361,8 +422,7 @@ void loop() {
         fi_Interface.set(0, 1);
       }else if(millis() - FI_G_MILLIS < 200*4){
         fi_Interface.set(0, 0);
-      }else if(millis() - FI_G_MILLIS < 200*5){
-        fi_Interface.set(1, 0);
+      }else{
         FI_G_MILLIS = millis();
       }
       if(FI_NAV_R){
@@ -379,16 +439,13 @@ void loop() {
         delay(200);
       }
       if(FI_NAV_L){
+        fi_Interface.set(1, 0);
         fi_IO.safe(FI_D_LEFT);
+        fi_Interface.set(0, 0);
         FI_G_MENU_STATE = 0;
         FI_G_MENU = FI_D_MENU_MENU;
         delay(200);
       }
     }
   }
-    // put your main code here, to run repeatedly:
-
-
-
-
 }
